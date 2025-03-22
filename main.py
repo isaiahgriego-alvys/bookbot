7279ecf812094ce35a7d09738556939de7ac0c79
isaiahgriego-alvys <isaiah.griego@alvys.com>
2025-03-22 15:22:47 -0500

    feat: added printing of report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars, sort
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,6 +7,15 @@ def get_book_text(file_path):
 
 def main():
     file_contents = get_book_text("./books/frankenstein.txt")
-    print(f"{get_num_words(file_contents)} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(file_contents)} total words")
+    print("--------- Character Count -------")
+    dict = get_num_chars(file_contents)
+    sorted = sort(dict)
+    for x in sorted:
+        print(f'{x["name"]}: {x["num"]}')
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def get_num_words(file_contents):
    return len(file_contents.split())
+
+def get_num_chars(file_contents):
+    dict = {}
+    for char in file_contents:
+        lower = char.lower()
+        if dict.get(lower):
+            dict[lower] = dict[lower]+ 1
+        else:
+            dict[lower] = 1
+    return dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort(dict):
+    sorted = []
+    for x in dict:
+        if x.isalpha():
+            sorted.append({ "name": x, "num": dict[x]})
+    sorted.sort(reverse = True, key=sort_on)
+    return sorted
