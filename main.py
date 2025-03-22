a761473e3cb43654ee4a3478374f5b09e43a0dbc
isaiahgriego-alvys <isaiah.griego@alvys.com>
2025-03-22 14:52:10 -0500

    read some file data

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def count_words(file_contents):
+   return len(file_contents.split())
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    file_contents = get_book_text("./books/frankenstein.txt")
+    print(f"{count_words(file_contents)} words found in the document")
+
+main()
